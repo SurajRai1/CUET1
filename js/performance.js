cdf452ad36f38b527759c7711bf928b995c4a998
Suraj Rai <150836658+SurajRai1@users.noreply.github.com>
2024-02-18 02:42:20 +0530

    Update performance.js

--- js/performance.js
+++ js/performance.js
@@ -1,13 +1,30 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const scoresParameter = urlParams.get('scores');
+    scores = JSON.parse(decodeURIComponent(scoresParameter));
+
+    // Display the score
+    document.getElementById('percentage').textContent = `${scores.Percentage}%`;
+    document.getElementById('correct').textContent = `${scores['Correct Answers']}`;
+    document.getElementById('wrong').textContent = `${scores['Wrong Answers']}`;
+    document.getElementById('total').textContent = `${scores['Total Questions']}`;
+  
+    displayAnswers();
+
+    // Add "Back to Home" button
+    const backButton = document.getElementById('backButton');
+    backButton.addEventListener('click', goBack);
+});
+
 function getAnswerCounts() {
     let correctCount = 0;
     let incorrectCount = 0;
 
-    const questionsForSubject = questionsData[subjectDropdown.value];
-    const totalQuestions = questionsForSubject.length;
-
     for (let i = 1; i <= totalQuestions; i++) {
         const selectedOption = document.querySelector(`input[name="q${i}"]:checked`);
-        const questionData = questionsForSubject[i - 1];
+        const questionData = questionsData[subjectDropdown.value][i - 1];
 
         if (selectedOption) {
             const isCorrect = selectedOption.value === questionData.correctAnswer;
@@ -23,17 +40,15 @@ function getAnswerCounts() {
     }
 
     return { correctCount, incorrectCount };
-}
+};
 
 function displayAnswers() {
     const answersList = document.getElementById('answers-list');
-    const questionsForSubject = questionsData[subjectDropdown.value];
-    const totalQuestions = questionsForSubject.length;
 
     // Iterate through submitted answers (you may need to adapt this based on your data structure)
     for (let i = 1; i <= totalQuestions; i++) {
         const selectedOption = document.querySelector(`input[name="q${i}"]:checked`);
-        const questionData = questionsForSubject[i - 1];
+        const questionData = questionsData[subjectDropdown.value][i - 1];
 
         const answerItem = document.createElement('li');
         answerItem.innerHTML = `<p>${i}. ${questionData.question}</p>`;
@@ -55,4 +70,12 @@ function displayAnswers() {
 
         answersList.appendChild(answerItem);
     }
-}
+};
+
+
+
+function goBack() {
+    // Navigate back to the home page
+    window.location.href = '../index.html';
+    // alert(scores.Percentage)
+};
